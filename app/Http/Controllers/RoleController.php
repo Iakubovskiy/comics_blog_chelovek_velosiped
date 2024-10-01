934e30fa1b00c4b3165ec38211258fbd762742c5
Iakubovskiy <diman.150106@gmail.com>
2024-10-01 09:04:08 +0300

    TAB-9 git test

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -21,6 +21,7 @@ class RoleController extends Controller
     public function create()
     {
         return view('admin.roles.create');
+        //some test changes
     }
 
     public function store(Request $request)
